95d209db478eca3933b493e190b4c3278f62bb22
Sarah Situ <sarahsitu563@gmail.com>
2024-11-28 12:33:21 +1300

    Updating URL with search params

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,11 +1,26 @@
-'use client';
+"use client";
 
-import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import { URLSearchParams } from "next/dist/compiled/@edge-runtime/primitives/url";
+import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
-export default function Search({ placeholder }: { placeholder: string }) {
-  function handleSearch(term:string) {
-    console.log(term)
+export default function Search(placeholder:string) {
+  const SearchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(SearchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+    // console.log(term);
   }
+  
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -15,7 +30,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
         placeholder={placeholder}
         onChange={(e) => {
-          handleSearch(e.target.value)
+          handleSearch(e.target.value);
         }}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
