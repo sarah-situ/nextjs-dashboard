9cb02f14983e54f633c9c8a18fe55cd97079e317
Sarah Situ <sarahsitu563@gmail.com>
2024-11-28 15:07:45 +1300

    Implement Debouncing for invoice search query

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,23 +2,25 @@
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
 
-export default function Search({placeholder}:{placeholder:string}) {
+export default function Search({ placeholder }: { placeholder: string }) {
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
+    console.log(`Searching... ${term}`);
+
     const params = new URLSearchParams(searchParams);
     if (term) {
-      params.set('query', term);
+      params.set("query", term);
     } else {
-      params.delete('query');
+      params.delete("query");
     }
+
     replace(`${pathname}?${params.toString()}`);
-    // console.log(term);
-  }
-  
+  }, 300);
 
   return (
     <div className="relative flex flex-1 flex-shrink-0">
@@ -31,7 +33,7 @@ export default function Search({placeholder}:{placeholder:string}) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
-        defaultValue={searchParams.get('query')?.toString()}
+        defaultValue={searchParams.get("query")?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
