0d58818189721ba819737fcd93f8654d68e8b8ac
Sarah Situ <sarahsitu563@gmail.com>
2024-11-28 13:04:44 +1300

    Updating table component to reflect search query

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -1,16 +1,15 @@
 "use client";
 
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { URLSearchParams } from "next/dist/compiled/@edge-runtime/primitives/url";
 import { usePathname, useSearchParams, useRouter } from "next/navigation";
 
-export default function Search(placeholder:string) {
-  const SearchParams = useSearchParams();
+export default function Search({placeholder}:{placeholder:string}) {
+  const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
 
   function handleSearch(term: string) {
-    const params = new URLSearchParams(SearchParams);
+    const params = new URLSearchParams(searchParams);
     if (term) {
       params.set('query', term);
     } else {
@@ -32,6 +31,7 @@ export default function Search(placeholder:string) {
         onChange={(e) => {
           handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
