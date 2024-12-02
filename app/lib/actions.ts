7d2bf2bc43c6acc5ed44ced24eff5e32e98e0c7d
Sarah Situ <sarahsitu563@gmail.com>
2024-12-02 22:46:34 +1300

    Insert updated invoice data with server actions & redirect to invoice page

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,8 +1,8 @@
 "use server";
 import { z } from "zod"; //typescript validation library
-import { sql } from '@vercel/postgres'
+import { sql } from "@vercel/postgres";
 import { revalidatePath } from "next/cache";
-import { redirect } from 'next/navigation'
+import { redirect } from "next/navigation";
 
 const FormSchema = z.object({
   id: z.string(),
@@ -21,13 +21,31 @@ export async function createInvoice(formData: FormData) {
     status: formData.get("status"),
   });
   const amountInCents = amount * 100;
-  const date = new Date().toISOString().split('T')[0];
+  const date = new Date().toISOString().split("T")[0];
 
   await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})`;
 
-    revalidatePath('/dashboard/invoices');
-    redirect('/dashboard/invoices');
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
+}
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+  const amountInCents = amount * 100;
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}`;
+
+  revalidatePath("/dashboard/invoices");
+  redirect("/dashboard/invoices");
 }
 
