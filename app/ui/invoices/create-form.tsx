2fb066c594a1c726d0016c403b70ebc97f5eecf0
Sarah Situ <sarahsitu563@gmail.com>
2024-12-07 19:28:06 +1300

    Access errors in create-form with state

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useActionState } from 'react';
+
 import { CustomerField } from '@/app/lib/definitions';
 import Link from 'next/link';
 import {
@@ -11,10 +11,12 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '@/app/ui/button';
 import { createInvoice, State } from '@/app/lib/actions';
+import { useActionState } from 'react';
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   const initialState: State = {message: null, errors: {} }
   const [state, formAction]= useActionState(createInvoice, initialState)
+  console.log(state)
   return (
     <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
@@ -29,6 +31,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby='customer-error'
             >
               <option value="" disabled>
                 Select a customer
@@ -37,9 +40,18 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 <option key={customer.id} value={customer.id}>
                   {customer.name}
                 </option>
+          
               ))}
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+            <div id="customer-error" aria-live="polite" aria-atomic="true">
+        {state.errors?.customerId &&
+          state.errors.customerId.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+          ))}
+      </div>
           </div>
         </div>
 
