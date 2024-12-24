f9352c2fed07350af17c49afb596ea0bb344fa8f
Sarah Situ <sarahsitu563@gmail.com>
2024-12-24 20:47:38 +1300

    Fix linting error

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,7 +2,6 @@ import Form from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
 import { notFound } from "next/navigation";
-import { customers } from "@/app/lib/placeholder-data";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
